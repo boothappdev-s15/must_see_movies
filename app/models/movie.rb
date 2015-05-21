33f8e4faf21a05c7c6d21ef30f03db1871e011d4
Raghu Betina <rbetina@github.com>
2015-05-20 19:47:29 -0500

    Switches to belongs_to

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  belongs_to :director
+
+  # belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
+
+  # def director
+  #   return Director.find_by({ :id => self.director_id })
+  # end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,14 @@
 class Role < ActiveRecord::Base
+
+  belongs_to :actor
+  belongs_to :movie
+
+  # def actor
+  #   return Actor.find_by({ :id => self.actor_id })
+  # end
+
+  # def movie
+  #   return Movie.find_by({ :id => self.movie_id })
+  # end
+
 end
