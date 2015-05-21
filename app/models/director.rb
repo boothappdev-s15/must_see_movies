537a57b08427f75144c06a81ae1f33c9b61f97d5
Raghu Betina <rbetina@github.com>
2015-05-20 20:38:47 -0500

    Shows filmography on directors#show

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,12 @@
 class Director < ActiveRecord::Base
+
+  has_many :movies
+
+  # has_many :movies, :class_name => "Movie", :foreign_key => "director_id"
+
+  # Set up the has_many for movies => roles
+  # Set up the has_many for actors => roles
+  # Use the new methods you get to display a list of character names
+  #   for each movie on the movies#show page
+  # Similarly, for actors on the actors#show page
 end
