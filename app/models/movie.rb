b72285138bb2c4060e1af3797de64d31e335073c
Raghu Betina <rbetina@github.com>
2015-05-20 20:24:46 -0500

    Completes dropdowns

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :director, :presence => true
+
   belongs_to :director
 
   # belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ActiveRecord::Base
+  validates :actor, :presence => true
+  validates :movie, :presence => true
 
   belongs_to :actor
   belongs_to :movie
